05020de67d2a9b13927be74f1a1e0a6fce9fa3e9
DalmiroFerrero <dalmirofb@gmail.com>
2021-03-19 09:52:59 -0300

    Full responsive

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,26 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
 import logo from '../assets/logo.png';
+import { FaBars } from 'react-icons/fa';
 
 const NavBar = () => {
   const [navBar, setNavBar] = useState(false);
+  const [navBarMovil, setNavBarMovil] = useState(false);
+
+  const changeNavBarMovil = () => {
+    setNavBarMovil(!navBarMovil);
+  };
 
   const changeBackgroundNavBar = () => {
     if (window.scrollY >= 70) {
       setNavBar(true);
     } else {
       setNavBar(false);
+      setNavBarMovil(false);
     }
   };
 
   window.addEventListener('scroll', changeBackgroundNavBar);
 
   return (
-    <nav className={`navbar-background ${navBar ? 'background-nav' : ''}`}>
+    <nav
+      className={`${
+        navBarMovil
+          ? 'navbar-background-menu background-nav'
+          : 'navbar-background'
+      } ${navBar ? 'background-nav' : ''}`}
+    >
       <div className="navbar">
         <Link to="header" smooth={true} duration={1000}>
           <img alt="Logo" src={logo} />
         </Link>
+
+        <div className="icon-bars">
+          <FaBars onClick={changeNavBarMovil} />
+        </div>
 
         <ul class="navegacion">
           <li>
